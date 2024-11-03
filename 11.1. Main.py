61c7ea738c0dc3f987ced176c71b8ed88e7265cf
Cathrine Nesterova <nev.hrd@gmail.com>
2024-11-03 15:26:46 +0300

    Добавлен интерфейс, исправлен вывод сообщения не в консоль, а в метку.

--- 11.1. Main.py
+++ 11.1. Main.py
@@ -1,27 +1,65 @@
 import smtplib
 from email.message import EmailMessage
+from tkinter import *
 
-sender_email = 'maya.v.grant@yandex.ru'
-recipient_mail = 'almier@bk.ru'
-password = 'ndtxbqnryqxbkriv'
-subject = 'Проверка связи'
-body = 'Привет из программы на Питоне'
 
-msg = EmailMessage()
-msg.set_content(body)
-msg['Subject'] = subject
-msg['From'] = sender_email
-msg['To'] = recipient_mail
+def send_email():
+    sender_email = 'maya.v.grant@yandex.ru'
+    recipient_mail = 'almier@bk.ru'
+    password = 'ndtxbqnryqxbkriv'
+    subject = 'Проверка связи'
+    body = 'Привет из программы на Питоне'
 
-try:
-    server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
-    server.login(sender_email, password)
-    server.send_message(msg)
-    print('Письмо отправлено!')
-except Exception as e:
-    print(f'Ошибка: {e}')
-#  finally - то, что будет выполнено в любом случае, возникнет исключение или нет. Здесь выключает сервер.
-finally:
-    if server:
-        server.quit()
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = recipient_mail
 
+    server = None
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+        server.login(sender_email, password)
+        server.send_message(msg)
+        result_label.config(text='Письмо отправлено!')
+    except Exception as e:
+        result_label.config(text=f'Ошибка: {e}')
+    #  finally - то, что будет выполнено в любом случае, возникнет исключение или нет. Здесь выключает сервер.
+    finally:
+        if server:
+            server.quit()
+
+# Создание главного окна
+window = Tk()
+window.title('Отправка e-mail')
+window.geometry('500x300')
+
+# Создание и размещение виджетов
+Label(text="Отправитель (Email):").grid(row=0, column=0, sticky=W)
+sender_email_entry = Entry(window)
+sender_email_entry.grid(row=0, column=1, sticky=W)
+
+Label(text="Получатель (Email):").grid(row=1, column=0, sticky=W)
+recipient_email_entry = Entry(window)
+recipient_email_entry.grid(row=1, column=1, sticky=W)
+
+Label(text="Пароль приложения:").grid(row=2, column=0, sticky=W)
+password_entry = Entry(show="*")
+password_entry.grid(row=2, column=1, sticky=W)
+
+Label(text="Тема письма:").grid(row=3, column=0, sticky=W)
+subject_entry = Entry(window)
+subject_entry.grid(row=3, column=1, sticky=W)
+
+Label(text="Сообщение:").grid(row=4, column=0, sticky=N)
+body_text = Text(height=10, width=45)
+body_text.grid(row=4, column=1)
+
+Button(text="Отправить", command=send_email).grid(row=5, column=1, sticky=E)
+
+result_label = Label(text="")
+result_label.grid(row=6, column=1, sticky=W)
+
+# Запуск главного цикла окна
+window.mainloop()
